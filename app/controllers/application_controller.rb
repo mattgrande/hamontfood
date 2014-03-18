060e3c35ab08ae03a4594d95e224c8f0681c899c
Matt Grande <matt@weeverapps.com>
2014-03-18 16:08:56 -0400

    Add counts to the footer.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_aggregate_data
+
+private
+  def set_aggregate_data
+  	@premise_count = Premise.count
+  	@inspection_count = Inspection.count
+  end
 end
